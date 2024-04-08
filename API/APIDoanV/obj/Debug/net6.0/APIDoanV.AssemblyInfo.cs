8f9e29388580c94ef7729fc3ec6578e1eeb75483
= <nmanh230101@gmail.com>
2024-04-08 18:54:36 +0700

    update code

--- API/APIDoanV/obj/Debug/net6.0/APIDoanV.AssemblyInfo.cs
+++ API/APIDoanV/obj/Debug/net6.0/APIDoanV.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIDoanV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e225faa3badfbb130e868598a6a69f86b82e41a")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIDoanV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIDoanV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
